37c7bd51365d605f731b77eac8e58ce33984923d
Sean Morrison <seanmorrison449@gmail.com>
2017-08-31 23:57:31 +1000

    Update main.py
    Time series solution and plot added. Still problems with solution going unstable -- looks like the aircraft slides backwards and everything blows up. Might be possible to fix this with full-envelope airfoil data (at the moment, linearized data is used, and CL drops to zero past the stall angle).

--- Aero Strip Theory/main.py
+++ Aero Strip Theory/main.py
@@ -1,7 +1,10 @@
-import forcecalc
+import forcecalc as fc
 import numpy as np
 import math
 from scipy.interpolate import RectBivariateSpline
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 U2Xvals = np.loadtxt('Xvals')
 U2Yvals = np.loadtxt('Yvals')
@@ -25,12 +28,12 @@ g = 9.81
 'fuselage geometry'
 proprad = 0.17
 fuserad = 0.125
-x_cg = 0.25
+x_cg = 0.45
 A_b_ref = math.pi * fuserad * fuserad
 m = 2.85
-Ixx = 1.5
-Iyy = 1.8
-Izz = 1.5
+Ixx = 1
+Iyy = 3
+Izz = 1
 
 'propeller disk area'
 diskA = math.pi * proprad * proprad
@@ -47,7 +50,7 @@ alphamax = 20
 '########################## WING ############################'
 'wing geometry'
 wspan = 1.8
-winc = 3
+winc = 1.5
 rc_w = 0.35
 tc_w = 0.11
 cbar_w = (rc_w + tc_w) / 2
@@ -73,13 +76,13 @@ e_w = 0.8
 '########################## HORIZONTAL TAIL ############################'
 'horizontal tail geometry'
 htspan = 0.65
-htinc = 0
+htinc = -2
 rc_ht = 0.25
 tc_ht = 0.25
 cbar_ht = (rc_ht+tc_ht)/2
 Sref_ht = cbar_ht*htspan
 qtc_sweep_ht = 0
-htail_root_le_x = 0.95
+htail_root_le_x = 1.15
 AR_ht = htspan*htspan/Sref_ht
 ht_el = htspan/2/steps
 
@@ -149,18 +152,36 @@ phi = 0
 theta = 0
 psi = 0
 
-forcecalc.setuvals(U2Xvals, U2Yvals, U2Zvals, U2func)
-forcecalc.setsolverparams(timestep, udot, vdot, wdot, pdot, qdot, rdot, steps, rho, g)
-forcecalc.setacparams(m, Ixx, Iyy, Izz, proprad, fuserad, x_cg, CD0_b, dCDb_dB, dCDb_dA, alphamin, alphamax)
-forcecalc.setwingparams(wspan, winc, rc_w, tc_w, qtc_sweep_w, wing_root_le_x, dCL_da_w, dCL_de_w, CL0_w, CD0_w, Y_w, y_w, e_w)
-forcecalc.sethtailparams(htspan, htinc, rc_ht, tc_ht, qtc_sweep_ht, htail_root_le_x, dCL_da_ht, dCL_de_ht, CL0_ht, CD0_ht, Y_ht, y_ht, e_ht)
-forcecalc.setvtailparams(vtspan, vtinc, rc_vt, tc_vt, qtc_sweep_vt, vtail_root_le_x, dCL_da_vt, dCL_de_vt, CL0_vt, CD0_vt, Y_vt, y_vt, e_vt)
-forcecalc.buildgeom()
+fc.setuvals(U2Xvals, U2Yvals, U2Zvals, U2func)
+fc.setsolverparams(timestep, udot, vdot, wdot, pdot, qdot, rdot, steps, rho, g)
+fc.setacparams(m, Ixx, Iyy, Izz, proprad, fuserad, x_cg, CD0_b, dCDb_dB, dCDb_dA, alphamin, alphamax)
+fc.setwingparams(wspan, winc, rc_w, tc_w, qtc_sweep_w, wing_root_le_x, dCL_da_w, dCL_de_w, CL0_w, CD0_w, Y_w, y_w, e_w)
+fc.sethtailparams(htspan, htinc, rc_ht, tc_ht, qtc_sweep_ht, htail_root_le_x, dCL_da_ht, dCL_de_ht, CL0_ht, CD0_ht, Y_ht, y_ht, e_ht)
+fc.setvtailparams(vtspan, vtinc, rc_vt, tc_vt, qtc_sweep_vt, vtail_root_le_x, dCL_da_vt, dCL_de_vt, CL0_vt, CD0_vt, Y_vt, y_vt, e_vt)
+fc.buildgeom()
 
-forces = forcecalc.forcecalc(power, u, v, w, p, q, r, aileron, elevator, rudder)
-coefficients = forcecalc.coefs(u, v, w, forces)
-state = forcecalc.nlti(u, v, w, p, q, r, x, y, z, phi, theta, psi, forces)
+steps = 100
+X, V, W, THETA = np.zeros((steps,3)), np.zeros((steps,3)), np.zeros((steps,3)), np.zeros((steps,3))
+X[0][0], X[0][1], X[0][2]  = x, y, z
+V[0][0], V[0][1], V[0][2]  = u, v, w
+W[0][0], W[0][1], W[0][2]  = p, q, r
+THETA[0][0], THETA[0][1], THETA[0][2]  = phi, theta, psi
 
-print forces
-print coefficients
-print state
+for i in range(1, steps):
+    forces = fc.forcecalc(power, u, v, w, p, q, r, aileron, elevator, rudder)
+    coefficients = fc.coefs(u, v, w, forces)
+    state = fc.nlti(u, v, w, p, q, r, x, y, z, phi, theta, psi, forces)
+    X[i][0], X[i][1], X[i][2] = state[6], state[7], state[8]
+    V[i][0], V[i][1], V[i][2] = state[0], state[1], state[2]
+    W[i][0], W[i][1], W[i][2] = state[3], state[1], state[2]
+    THETA[i][0], THETA[i][1], THETA[i][2] = state[9], state[10], state[11]
+    u, v, w, p, q, r = state[0], state[1], state[2], state[3], state[4], state[5]
+    x, y, z, phi, theta, psi = state[6], state[7], state[8], state[9], state[10], state[11]
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+x = X[0:steps,0]
+y = X[0:steps,1]
+z = X[0:steps,2]
+ax.plot(x, y, z)
+plt.show()
